7f6268af82387d35bfbaf909ac40f70c2d3c7e7d
Franck De Goër <degoerdf@ensimag.fr>
2014-06-07 17:28:54 +0200

    Improve UI

--- color/static/color/js/colorify.js
+++ color/static/color/js/colorify.js
@@ -1,4 +1,18 @@
 
+/* We define here some referenced colors used */
+
+/* Border color */
+var border_color = new Array(0x34, 0x49, 0x5e);
+/* Initial color */
+var base_color = new Array(0xbd, 0xc3, 0xc7);
+/* Coloration color */
+var propag_color = new Array(0x34, 0x98, 0xdb);
+
+
+var color_to_string = function(color) {
+    return "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")";
+}
+
 
 /*  
     Get coordinates of a cell 
@@ -29,16 +43,9 @@ var cell_at = function(x, y) {
     Color the cell with the r, g, b values given in parameter
 */
 var rgb = function(cell, r, g, b) {
-    cell.css("color", "rgb(" + r + ", " + g + ", " + b + ")");
+    cell.css("color", color_to_string([r, g, b]));
+    //"rgb(" + r + ", " + g + ", " + b + ")");
 };
-
-/* Original color composants of the squares on the map */
-var or_r = 189;
-var or_g = 195;
-var or_b = 199;
-/* Original color of the squares on the map as a string */
-var original_color = "rgb(" + or_r + ", " + or_g + ", " + or_b + ")";
-
 
 /* 
     Propagation of the color by diffusion algorithm
@@ -52,7 +59,7 @@ var propagate = function(row, col, pair, r, g, b) {
         if (cell.length == 0) return;
 
         /* If the cell is already colored, do nothing */
-        if (cell.css("color") != original_color) {
+        if (cell.css("color") != color_to_string(base_color)) {
             return;
         }
 
@@ -111,31 +118,31 @@ var draw_line = function(start, stop) {
     console.log(stop_coord);
     console.log(senti_coord);
 
-    rgb(start, 0, 0, 0);
+    rgb(start, border_color[0], border_color[1], border_color[2]);
     while (senti_coord[0] != stop_coord[0]) {
         console.log(senti_coord);
         senti_coord[0] += dirx;
-        rgb(cell_at(senti_coord[0], senti_coord[1]), 0, 0, 0);
+        rgb(cell_at(senti_coord[0], senti_coord[1]), border_color[0], border_color[1], border_color[2]);
     }
     while (senti_coord[1] != stop_coord[1]) {
         console.log(senti_coord);
         senti_coord[1] += diry;
-        rgb(cell_at(senti_coord[0], senti_coord[1]), 0, 0, 0);
+        rgb(cell_at(senti_coord[0], senti_coord[1]), border_color[0], border_color[1], border_color[2]);
     }
 }
 
 var clean_mess = function() {
     $(".square").each(function() {
-        if ($( this ).css("color") != "rgb(0, 0, 0)") {
-            rgb($( this ), or_r, or_g, or_b);
+        if ($( this ).css("color") != color_to_string(border_color)) {
+            rgb($( this ), base_color[0], base_color[1], base_color[2]);
         }
     });
 }
 
 var clean_borders = function() {
     $(".square").each(function() {
-        if ($( this ).css("color") == "rgb(0, 0, 0)") {
-            rgb($( this ), or_r, or_g, or_b);
+        if ($( this ).css("color") == color_to_string(border_color)) {
+            rgb($( this ), base_color[0], base_color[1], base_color[2]);
         }
     });
 }
@@ -145,22 +152,34 @@ var starting_point = null;
 $("#clean-btn").click(clean_mess);
 $("#clean-borders-btn").click(clean_borders);
 
+$("input:radio[name=edition-rb]:radio").change(function() {
+    if ($("input:radio[name=edition-rb]:checked").val() == 1) {
+        $("#color-area").removeClass("edition");
+    } else {
+        $("#color-area").addClass("edition");
+    }
+
+    return;
+});
+
 $(".square").click(function() {
     switch ($("input:radio[name=edition-rb]:checked").val()) {
         case "1":
+            $("#color-area").removeClass("not-started");
             coord = coordinates($( this ));
-            propagate(coord[0], coord[1], true, 0x34, 0x98, 0xdb);
+            propagate(coord[0], coord[1], true, propag_color[0], propag_color[1], propag_color[2]);
+            $("#color-area").addClass("not-started");
             break;
         case "2":
-            if ($( this ).css("color") == "rgb(0, 0, 0)") {
-                rgb($( this ), or_r, or_g, or_b);
+            if ($( this ).css("color") == color_to_string(border_color)) {
+                rgb($( this ), base_color[0], base_color[1], base_color[2]);
             } else {
-                rgb($( this ), 0, 0, 0);
+                rgb($( this ), border_color[0], border_color[1], border_color[2]);
             }
             break;
         case "3":
-            console.log("Draw line.");
             if (starting_point == null) {
+                rgb($( this ), border_color[0], border_color[1], border_color[2]);
                 starting_point = $( this );
             } else {
                 draw_line(starting_point, $( this ));
